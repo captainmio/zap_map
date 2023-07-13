823afd70de524cb7296559a5c1644bdc7013ccc1
Rustum Jordan <rustumjordan@gmail.com>
2023-07-14 00:02:52 +0800

    code refactor

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -10,14 +10,18 @@ class LocationController extends Controller
       
       // didn't add request here as the value are within URL parameters
       if(is_numeric($latitude) && is_numeric($longitude) && preg_match('/^\d+$/',$radius) && $radius > 0) {
+
         $locations = $getlocations->getLocationsWithinRadius($latitude, $longitude, $radius);
+        $response = $locations;
 
-        return response()->json($locations);
       } else {
-        return response()->json(array("error" => true,
-      "message" => "Invalid parameters"));
-      }
 
+        $response = array("error" => true,
+        "message" => "Invalid parameters");
+
+      }
+      
+      return response()->json($response);
       
     }
 }
