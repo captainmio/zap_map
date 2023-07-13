6b46669db4a45496bbbcf6bfc2b348c8cbe62498
Rustum Jordan <rustumjordan@gmail.com>
2023-07-13 23:49:30 +0800

    add comment

--- app/Services/Locations/GetLocations.php
+++ app/Services/Locations/GetLocations.php
@@ -5,6 +5,8 @@ use App\Models\Location;
 
 class GetLocations {
   public function getLocationsWithinRadius($latitude, $longitude, $radius) {
+
+    // sorry I have to use a package to calculate radius. I am dumb with math and calculation ;p #peace
     $locations = Location::radius($latitude, $longitude, $radius)->get();
 
     return $locations;
