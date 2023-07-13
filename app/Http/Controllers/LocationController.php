7b0424bd48a6d88f66cdb761ba3a48b829e759af
Rustum Jordan <rustumjordan@gmail.com>
2023-07-14 00:01:36 +0800

    add validation

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -8,8 +8,16 @@ class LocationController extends Controller
 {
     public function get_locations(GetLocations $getlocations, $latitude, $longitude, $radius) {
       
-      $locations = $getlocations->getLocationsWithinRadius($latitude, $longitude, $radius);
+      // didn't add request here as the value URL parameters
+      if(is_numeric($latitude) && is_numeric($longitude) && preg_match('/^\d+$/',$radius) && $radius > 0) {
+        $locations = $getlocations->getLocationsWithinRadius($latitude, $longitude, $radius);
 
-      return response()->json($locations);
+        return response()->json($locations);
+      } else {
+        return response()->json(array("error" => true,
+      "message" => "Invalid parameters"));
+      }
+
+      
     }
 }
