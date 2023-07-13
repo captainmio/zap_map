088b54632c49e8b3dd39a7acf5a12d6a8f035237
Rustum Jordan <rustumjordan@gmail.com>
2023-07-14 00:01:58 +0800

    update a comment

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -8,7 +8,7 @@ class LocationController extends Controller
 {
     public function get_locations(GetLocations $getlocations, $latitude, $longitude, $radius) {
       
-      // didn't add request here as the value URL parameters
+      // didn't add request here as the value are within URL parameters
       if(is_numeric($latitude) && is_numeric($longitude) && preg_match('/^\d+$/',$radius) && $radius > 0) {
         $locations = $getlocations->getLocationsWithinRadius($latitude, $longitude, $radius);
 
